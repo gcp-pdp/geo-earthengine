141bad09695edc62e1a1941387d7c6c3ed4a454f
Allen Day <allenday@users.noreply.github.com>
2021-05-10 16:56:51 +0800

    Create NOAA_CFSV2_FOR6H.sql

---
+++ tables/NOAA_CFSV2_FOR6H.sql
@@ -0,0 +1,134 @@
+[
+    {
+        "name": "Downward_Long_Wave_Radp_Flux_surface_6_Hour_Average",
+        "type": "FLOAT",
+        "mode": "REQUIRED",
+        "description": "Downward long-wave radiation flux at surface, 6-hour average. W/m^2"
+    },
+    {
+        "name": "Upward_Long_Wave_Radp_Flux_surface_6_Hour_Average",
+        "type": "FLOAT",
+        "mode": "REQUIRED",
+        "description": ""
+    },
+    {
+        "name": "Downward_Short_Wave_Radiation_Flux_surface_6_Hour_Average",
+        "type": "FLOAT",
+        "mode": "REQUIRED",
+        "description": ""
+    },
+    {
+        "name": "Upward_Short_Wave_Radiation_Flux_surface_6_Hour_Average",
+        "type": "FLOAT",
+        "mode": "REQUIRED",
+        "description": ""
+    },
+    {
+        "name": "Geopotential_height_surface",
+        "type": "FLOAT",
+        "mode": "REQUIRED",
+        "description": "Geopotential height at surface. gpm"
+    },
+    {
+        "name": "Latent_heat_net_flux_surface_6_Hour_Average",
+        "type": "FLOAT",
+        "mode": "REQUIRED",
+        "description": "Latent heat net flux at surface, 6-hour average. W/m^2"
+    },
+    {
+        "name": "Minimum_specific_humidity_at_2m_height_above_ground_6_Hour_Interval",
+        "type": "FLOAT",
+        "mode": "REQUIRED",
+        "description": "Minimum specific humidity 2m above ground, 6-hour interval. kg/kg"
+    },
+    {
+        "name": "Maximum_specific_humidity_at_2m_height_above_ground_6_Hour_Interval",
+        "type": "FLOAT",
+        "mode": "REQUIRED",
+        "description": "Maximum specific humidity 2m above ground, 6-hour interval. kg/kg"
+    },
+    {
+        "name": "Minimum_temperature_height_above_ground_6_Hour_Interval",
+        "type": "FLOAT",
+        "mode": "REQUIRED",
+        "description": "Minimum temperature 2m above ground, 6-hour interval. K"
+    },
+    {
+        "name": "Maximum_temperature_height_above_ground_6_Hour_Interval",
+        "type": "FLOAT",
+        "mode": "REQUIRED",
+        "description": "Maximum temperature 2m above ground, 6-hour interval. K"
+    },
+    {
+        "name": "Potential_Evaporation_Rate_surface_6_Hour_Average",
+        "type": "FLOAT",
+        "mode": "REQUIRED",
+        "description": "Potential evaporation rate at surface, 6-hour average. W/m^2"
+    },
+    {
+        "name": "Precipitation_rate_surface_6_Hour_Average",
+        "type": "FLOAT",
+        "mode": "REQUIRED",
+        "description": "Precipitation rate at surface, 6-hour average. kg/m^2/s^1"
+    },
+    {
+        "name": "Pressure_surface",
+        "type": "FLOAT",
+        "mode": "REQUIRED",
+        "description": "Pressure at surface. Pa"
+    },
+    {
+        "name": "Sensible_heat_net_flux_surface_6_Hour_Average",
+        "type": "FLOAT",
+        "mode": "REQUIRED",
+        "description": "Sensible heat net flux at surface, 6-hour average. W/m^2"
+    },
+    {
+        "name": "Specific_humidity_height_above_ground",
+        "type": "FLOAT",
+        "mode": "REQUIRED",
+        "description": "Specific humidity 2m above ground. kg/kg"
+    },
+    {
+        "name": "Temperature_height_above_ground",
+        "type": "FLOAT",
+        "mode": "REQUIRED",
+        "description": "Temperature 2m above ground. K"
+    },
+    {
+        "name": "Volumetric_Soil_Moisture_Content_depth_below_surface_layer_5_cm",
+        "type": "FLOAT",
+        "mode": "REQUIRED",
+        "description": "Volumetric soil moisture content 5cm below surface layer. fraction"
+    },
+    {
+        "name": "Volumetric_Soil_Moisture_Content_depth_below_surface_layer_25_cm",
+        "type": "FLOAT",
+        "mode": "REQUIRED",
+        "description": "Volumetric soil moisture content 25cm below surface layer. fraction"
+    },
+    {
+        "name": "Volumetric_Soil_Moisture_Content_depth_below_surface_layer_70_cm",
+        "type": "FLOAT",
+        "mode": "REQUIRED",
+        "description": "Volumetric soil moisture content 70cm below surface layer. fraction"
+    },
+    {
+        "name": "Volumetric_Soil_Moisture_Content_depth_below_surface_layer_150_cm",
+        "type": "FLOAT",
+        "mode": "REQUIRED",
+        "description": "Volumetric soil moisture content 150cm below surface layer. fraction"
+    },
+    {
+        "name": "u_component_of_wind_height_above_ground",
+        "type": "FLOAT",
+        "mode": "REQUIRED",
+        "description": "U-component of wind 10m above ground. m/s"
+    },
+    {
+        "name": "v_component_of_wind_height_above_ground",
+        "type": "FLOAT",
+        "mode": "REQUIRED",
+        "description": "V-component of wind 10m above ground. m/s"
+    }
+]
